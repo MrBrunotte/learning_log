2ef76e5ea61b3becaab7290ab0d1189bca3ec1c4
mrbrunotte <mrbrunotte@gmail.com>
2020-08-05 19:58:27 +0200

    Add homepage, urls and models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -8,7 +8,25 @@ class Topic(models.Model):
     text = models.CharField(max_length=200)
     date_added = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        """Return a string representation of the model"""
+        return self.text
 
-def __str__(self):
-    """Return a string representation of the model"""
-    return self.text
+
+class Entry(models.Model):
+    """Something specific learned about a topic."""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string representation of the model"""
+        small_text = self.text[:50]
+
+        if self.text <= small_text[:50]:
+            return f"{self.text}"
+        else:
+            return f"{self.text[:50]}..."
